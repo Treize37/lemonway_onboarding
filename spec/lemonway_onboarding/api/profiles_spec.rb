d0754426b1150d922e4de8419cd38ebea3d27607
Ludovic Pelletier <ludovic.pelletier@gmail.com>
2025-11-24 13:36:46 +0100

    Adds new class Profiles with create method #4

---
+++ spec/lemonway_onboarding/api/profiles_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+require 'lemonway_onboarding/api/profiles'
+
+RSpec.describe LemonwayOnboarding::Api::Profiles do
+  let(:client) { instance_double(LemonwayOnboarding::Client) }
+  subject(:profiles_api) { described_class.new(client) }
+  describe '#create' do
+    it 'calls client.post with the correct endpoint and body' do
+      expect(client).to receive(:post).with('accounts/individual/123/profiles', { foo: 'bar' })
+      profiles_api.create('individual', '123', { foo: 'bar' })
+    end
+  end
+end
